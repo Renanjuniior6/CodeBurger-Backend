cf24faea1cc9020e1d9af8531027acadc25afddb
Renanjuniior6 <renanjrr679@gmail.com>
2023-11-07 10:40:15 -0300

    ajustes no caminho

--- src/app/controllers/models/Category.js
+++ src/app/controllers/models/Category.js
@@ -9,7 +9,7 @@ class Category extends Model {
         url: {
           type: Sequelize.VIRTUAL,
           get () {
-            return `http://localhost:3001/category-file/${this.path}`
+            return `https://codeburger-backend-production-12f3.up.railway.app/category-file/${this.path}`
           }
         }
       },
--- src/app/controllers/models/Product.js
+++ src/app/controllers/models/Product.js
@@ -11,7 +11,7 @@ class Product extends Model {
         url: {
           type: Sequelize.VIRTUAL,
           get () {
-            return `http://localhost:3001/product-file/${this.path}`
+            return `https://codeburger-backend-production-12f3.up.railway.app/product-file/${this.path}`
           }
         }
       },
